c746a743f9fe886237d4a638231dae252202bd7f
AlfaRiza <malfariza45@gmail.com>
2024-04-19 10:47:13 +0700

    feat: add middleware

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response , Application } from 'express';
 import dotenv from 'dotenv';
+import auth from './middleware/auth';
 
 //For env File 
 dotenv.config();
@@ -7,6 +8,7 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+app.use(auth)
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
